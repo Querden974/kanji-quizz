f53c400b4da306f5ff21b5da32dd5a28358de9f4
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-01-31 19:50:36 +0100

    import store and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Card from "./components/Card";
+import Liste from "./components/Liste";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getData } from "./features/kanjiApi";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const dispatch = useDispatch();
+  const kanji = useSelector((state) => state.api);
+
+  if (!kanji.loading && !kanji.error && !kanji.data) {
+    dispatch(getData());
+  }
+
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-base-200">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-base-200 gap-4">
+        <Liste />
         <Card />
       </div>
     </>
