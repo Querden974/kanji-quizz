6f3b719debbaf754ff9c65ddc3b8de48e2de8216
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-02-01 13:43:28 +0100

    Edit slice to dynamic api url

--- src/features/kanjiApi.js
+++ src/features/kanjiApi.js
@@ -1,11 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  urlApi: "",
   loading: false,
   data: undefined,
   error: false,
 };
-const apiUrl = "https://kanjiapi.dev/v1/kanji/grade-1";
+// const apiUrl = "https://kanjiapi.dev/v1/kanji/grade-1";
 
 const kanjiApi = createSlice({
   name: "kanjiApi",
@@ -22,26 +23,33 @@ const kanjiApi = createSlice({
     addLoading: (state, action) => {
       state.loading = true;
     },
+    setUrlApi: (state, action) => {
+      state.urlApi = "https://kanjiapi.dev/v1/kanji/grade-" + action.payload;
+      console.log(state.urlApi);
+    },
   },
 });
 
 export function getData(action) {
   return (dispatch, getState) => {
-    dispatch(addLoading());
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("HTTP error");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        dispatch(addData(data));
-      })
-      .catch((error) => dispatch(addError()));
+    if (getState().kanjiApi?.urlApi && getState().kanjiApi.urlApi.length > 0) {
+      dispatch(addLoading());
+      fetch(getState().kanjiApi.urlApi)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("HTTP error");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          dispatch(addData(data));
+          console.log(data);
+        })
+        .catch((error) => dispatch(addError()));
+    }
   };
 }
 
-export const { addData, addError, addLoading } = kanjiApi.actions;
+export const { addData, addError, addLoading, setUrlApi } = kanjiApi.actions;
 
 export default kanjiApi.reducer;
