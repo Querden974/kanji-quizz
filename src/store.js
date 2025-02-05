5c6cfc71fbdeb440519cb6ccdb22b3ea98ea5af8
QuerdenVFX <gautier.ray.vfx@gmail.com>
2025-02-05 17:51:43 +0100

    add player slice in store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import kanjiAPI from "./features/kanjiApi";
 import choices from "./features/choices";
 import settings from "./features/settings";
+import players from "./features/players";
 
 export const store = configureStore({
   reducer: {
     api: kanjiAPI, //Ajouter les reducers ici
     choices: choices,
     settings: settings,
+    players: players,
   },
 });
 
